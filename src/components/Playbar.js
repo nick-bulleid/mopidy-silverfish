f586f1e37306ac016995646b83e116e499e94f7e
Nick Bulleid <nedbulleid@fastmail.com>
2018-08-15 12:15:42 +0100

    Playbar work

--- src/components/Playbar.js
+++ src/components/Playbar.js
@@ -3,6 +3,29 @@ import './playbar.css';
 
 export default {
     view: function () {
-        return m(".playbar");
+        return m(".playbar", [
+            m(".ui.middle.aligned.equal.width.padded.grid", [
+                m(".row", [
+                    m(".controls.wide.column.center.aligned",
+                        m("button.ui.icon.button.blue.big.circular", [
+                            m("i.icon.backward")
+                        ]),
+                        m("button.ui.icon.button.blue.massive.circular", [
+                            m("i.icon.play")
+                        ]),
+                        m("button.ui.icon.button.blue.big.circular", [
+                            m("i.icon.forward")
+                        ])
+                    ),
+                    m(".column.left.aligned", [
+                        m("h1.ui.header.truncate.blue", [
+                            "Track Title",
+                            m(".sub.header.truncate", "by Artist from Album")
+                        ])
+                    ])
+                ]),
+                m(".row.blue")
+            ])
+        ]);
     }
 }
