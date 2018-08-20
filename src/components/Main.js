527b108f6d2b1f777cb687d29921c3d8c4f4469c
Nick Bulleid <nedbulleid@fastmail.com>
2018-08-20 20:06:24 +0100

    Basic Track item

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,24 @@
 import m from 'mithril';
 import './main.css'
 
+let Track = {
+    view: function () {
+        return m(".item", [
+            m(".content", [
+                m(".header", "Name")
+            ])
+        ])
+    }
+}
+
 export default {
     view: function () {
-        return m(".main");
+        return m(".main", [
+            m(".ui.container", [
+                m(".ui.items", [
+                    m(Track)
+                ])
+            ])
+        ]);
     }
 };
